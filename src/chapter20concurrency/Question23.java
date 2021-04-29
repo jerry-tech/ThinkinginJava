c414348523e5596243a540406d339592dd9175b7
Jeremiah Udoh <53123555+jerry-tech@users.noreply.github.com>
2021-04-29 20:30:00 +0100

    recent update

--- src/chapter20concurrency/Question23.java
+++ src/chapter20concurrency/Question23.java
@@ -1,4 +1,74 @@
 package chapter20concurrency;
 
-public class Question23 {
+import java.util.concurrent.*;
+
+//Exercise 23: (7) Demonstrate that WaxOMatic.java works successfully when you use
+//        notify( ) instead of notifyAll( ).
+
+class Car1 {
+    private boolean waxOn = false;
+    public synchronized void waxed() {
+        waxOn = true; // Ready to buff
+        // notifyAll();
+        notify();
+    }
+    public synchronized void buffed() {
+        waxOn = false; // Ready for another coat of wax
+        // notifyAll();
+        notify();
+    }
+    public synchronized void waitForWaxing() throws InterruptedException {
+        while(waxOn == false) wait();
+    }
+    public synchronized void waitForBuffing() throws InterruptedException {
+        while(waxOn == true) wait();
+    }
 }
+
+class WaxOn1 implements Runnable {
+    private Car1 car;
+    public WaxOn1(Car1 c) { car = c; }
+    public void run() {
+        try {
+            while(!Thread.interrupted()) {
+                System.out.print("Wax On! ");
+                TimeUnit.MILLISECONDS.sleep(200);
+                car.waxed();
+                car.waitForBuffing();
+            }
+        } catch(InterruptedException e) {
+            System.out.print("Exiting via interrupt");
+        }
+        System.out.print("Ending Wax On task");
+    }
+}
+
+class WaxOff1 implements Runnable {
+    private Car1 car;
+    public WaxOff1(Car1 c) { car = c; }
+    public void run() {
+        try {
+            while(!Thread.interrupted()) {
+                car.waitForWaxing();
+                System.out.print("Wax Off! ");
+                TimeUnit.MILLISECONDS.sleep(200);
+                car.buffed();
+            }
+        } catch(InterruptedException e) {
+            System.out.print("Exiting via interrupt");
+        }
+        System.out.print("Ending Wax Off task");
+    }
+}
+
+public class Question23 {
+    public static void main(String[] args) throws Exception {
+        Car1 car = new Car1();
+        ExecutorService exec = Executors.newCachedThreadPool();
+        exec.execute(new WaxOff1(car));
+        exec.execute(new WaxOn1(car));
+        TimeUnit.SECONDS.sleep(5); // Run for a while...
+        exec.shutdownNow(); // Interrupt all tasks
+    }
+}	
+ 
--- src/chapter20concurrency/Question25.java
+++ src/chapter20concurrency/Question25.java
@@ -1,4 +1,78 @@
 package chapter20concurrency;
 
+//Exercise 25: (1) In the Chef class in Restaurant.java, return from run( ) after
+//        calling shutdownNow( ) and observe the difference in behavior.
+
+import java.util.concurrent.*;
+
+
+class Meal {
+    private final int orderNum;
+    public Meal(int orderNum) { this.orderNum = orderNum; }
+    public String toString() { return "Meal " + orderNum; }
+}
+
+class WaitPerson25 implements Runnable {
+    private Question25 restaurant;
+    public WaitPerson25(Question25 r) { restaurant = r; }
+    public void run() {
+        try {
+            while(!Thread.interrupted()) {
+                synchronized(this) {
+                    while(restaurant.meal == null)
+                        wait(); // ... for the chef to produce a meal
+                }
+                System.out.print("WaitPerson25 got " + restaurant.meal);
+                synchronized(restaurant.chef) {
+                    restaurant.meal = null;
+                    restaurant.chef.notifyAll(); // ready for another
+                }
+            }
+        } catch(InterruptedException e) {
+            System.out.print("WaitPerson25 interrupted");
+        }
+    }
+}
+
+class Chef25 implements Runnable {
+    private Question25 restaurant;
+    private int count = 0;
+    public Chef25(Question25 r) { restaurant = r; }
+    public void run() {
+        try {
+            while(!Thread.interrupted()) {
+                synchronized(this) {
+                    while(restaurant.meal != null)
+                        wait(); // ... for the meal to be taken
+                }
+                if(++count == 10) {
+                    System.out.print("Out of food, closing");
+                    restaurant.exec.shutdownNow();
+                    return; // now Chef25 won't make another meal
+                }
+                System.out.print("Order up! ");
+                synchronized(restaurant.waitPerson) {
+                    restaurant.meal = new Meal(count);
+                    restaurant.waitPerson.notifyAll();
+                }
+                TimeUnit.MILLISECONDS.sleep(100);
+            }
+        } catch(InterruptedException e) {
+            System.out.print("Chef25 interrupted");
+        }
+    }
+}
+
 public class Question25 {
+    Meal meal;
+    ExecutorService exec = Executors.newCachedThreadPool();
+    WaitPerson25 waitPerson = new WaitPerson25(this);
+    Chef25 chef = new Chef25(this);
+    public Question25() {
+        exec.execute(chef);
+        exec.execute(waitPerson);
+    }
+    public static void main(String[] args) {
+        new Question25();
+    }
 }
